172a82dd6c18058543f6d2998fe8c2950502e8a3
Aielina <aielina.madambekova@gmail.com>
2024-10-03 18:04:51 +0600

    Added Toolbar for header

---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,11 @@
+const Toolbar = () => {
+  return (
+    <nav className="navbar bg-primary">
+      <div className="container">
+        <span className="navbar-brand mb-0 h1 text-white fs-1">Users</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
